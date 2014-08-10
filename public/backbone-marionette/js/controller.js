6f28dc64d8353a65ff9e35b8810804dbc161eb5d
Jonathan Kemp <kempdogg@gmail.com>
2014-08-09 23:01:54 -0400

    Initiate Controllers module.

--- public/backbone-marionette/js/controller.js
+++ public/backbone-marionette/js/controller.js
@@ -1,18 +1,16 @@
 /*global BackboneWizard, app, Marionette, $*/
+'use strict';
 
-BackboneWizard.Controllers = BackboneWizard.Controllers || {};
+app.module('Controllers', function (Controllers, app, Backbone, Marionette, $, _, BackboneWizard) {
 
-(function () {
-    'use strict';
-
-    BackboneWizard.Controllers.WizardController = Marionette.Controller.extend({
+    this.WizardController = Marionette.Controller.extend({
 
         index: function () {
             if (BackboneWizard.state !== 'success') {
 
                 BackboneWizard.wizardRouter.navigate('#/');
 
-                var itemView = new BackboneWizard.Views.ListView({ model: BackboneWizard.transaction, collection: BackboneWizard.itemList });
+                var itemView = new app.Views.ListView({ model: BackboneWizard.transaction, collection: BackboneWizard.itemList });
 
                 itemView.on('wizard:verify', this.showVerify, this);
 
@@ -27,7 +25,7 @@ BackboneWizard.Controllers = BackboneWizard.Controllers || {};
 
                 BackboneWizard.wizardRouter.navigate('#/verify');
 
-                var customerView = new BackboneWizard.Views.CustomerView({ model: BackboneWizard.transaction });
+                var customerView = new app.Views.CustomerView({ model: BackboneWizard.transaction });
 
                 customerView.on('wizard:payment', this.showPayment, this);
                 customerView.on('wizard:index', this.index, this);
@@ -43,7 +41,7 @@ BackboneWizard.Controllers = BackboneWizard.Controllers || {};
 
                 BackboneWizard.wizardRouter.navigate('#/payment');
 
-                var paymentView = new BackboneWizard.Views.PaymentView({ model: BackboneWizard.transaction });
+                var paymentView = new app.Views.PaymentView({ model: BackboneWizard.transaction });
 
                 paymentView.on('wizard:success', this.showSuccess, this);
                 paymentView.on('wizard:verify', this.showVerify, this);
@@ -55,29 +53,29 @@ BackboneWizard.Controllers = BackboneWizard.Controllers || {};
         },
 
         showSuccess: function () {
-            var state = BackboneWizard.state;
+            //var state = BackboneWizard.state;
 
-            if (state === 'success') {
+            if (BackboneWizard.state === 'success') {
 
                 BackboneWizard.wizardRouter.navigate('#/success');
 
-                var successView = new BackboneWizard.Views.SuccessView({ model: BackboneWizard.transaction });
+                var successView = new app.Views.SuccessView({ model: BackboneWizard.transaction });
 
                 app.wizard.show(successView);
             }
 
-            if (state === 'index') {
+            if (BackboneWizard.state === 'index') {
                 this.index();
             }
 
-            if (state === 'verify') {
+            if (BackboneWizard.state === 'verify') {
                 this.showVerify();
             }
 
-            if (state === 'payment') {
+            if (BackboneWizard.state === 'payment') {
                 this.showPayment();
             }
         }
 
     });
-})();
+}, BackboneWizard);
