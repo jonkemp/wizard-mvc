3e87e124f4587c4af5b16e6d1e3b69c9dc66658d
Jonathan Kemp <jkemp@scrippsnetworks.com>
2014-11-13 15:39:30 -0500

    Move bower_components directory to root.
    Tell express to mount the new location.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express();
 
 app.use( express.static( __dirname + '/public' ) );
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
